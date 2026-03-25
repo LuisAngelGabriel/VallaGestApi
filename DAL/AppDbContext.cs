d7f1ac7f8e6fbf0a2ef007e6be0edad08191dcae
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-25 13:04:07 -0500

    Crea el AppDbContext

---
+++ DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using VallaGestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VallaGestApi.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Valla> Vallas { get; set; }
+    }
+}
