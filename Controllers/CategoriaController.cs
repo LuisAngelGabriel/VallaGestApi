00870c7c7d25932f7ca51cfc878a25efea8d1047
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-25 13:03:42 -0500

    Crea el CategoriaController

---
+++ Controllers/CategoriaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VallaGestApi.DAL;
+using VallaGestApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VallaGestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetCategorias), new { id = categoria.CategoriaId }, categoria);
+        }
+    }
+}
