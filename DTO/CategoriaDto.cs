daa9e761f9a20a7924b614ab494b47919c0859d1
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-26 14:25:35 -0500

    Crea los DTOS de Valla y Categoria

---
+++ DTO/CategoriaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace VallaGestApi.DTO
+{
+    public class CategoriaDto
+    {
+        public int CategoriaId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+    }
+}
---
+++ DTO/VallaDto.cs
@@ -0,0 +1,14 @@
+﻿namespace VallaGestApi.DTO
+{
+    public class VallaDto
+    {
+        public int VallaId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public decimal PrecioMensual { get; set; }
+        public string? ImagenUrl { get; set; }
+        public bool EstaOcupada { get; set; }
+        public int CategoriaId { get; set; }
+        public string? CategoriaNombre { get; set; }
+    }
+}
