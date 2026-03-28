77ef2cd5a40ef526f49a052ec420a51bf41434d0
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:17:50 -0500

    Crea el CarritoDto

---
+++ DTO/CarritoDto.cs
@@ -0,0 +1,11 @@
+﻿namespace VallaGestApi.DTO
+{
+    public class CarritoItemDto
+    {
+        public int CarritoItemId { get; set; }
+        public int VallaId { get; set; }
+        public string NombreValla { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public string? ImagenUrl { get; set; }
+    }
+}
