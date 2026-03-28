8b0a2ed108e0af831bbcec4411a1d0280740a441
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:17:30 -0500

    Crea el modelo usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VallaGestApi.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+        [Required]
+        public string Rol { get; set; } = "Usuario";
+    }
+}
