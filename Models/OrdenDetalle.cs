d5458356d191ea609ea1fb7635ec864ecc3aae9b
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:17:22 -0500

    Crea el modelo OrdenDetalle

---
+++ Models/OrdenDetalle.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VallaGestApi.Models
+{
+    public class OrdenDetalle
+    {
+        [Key]
+        public int OrdenDetalleId { get; set; }
+        public int OrdenId { get; set; }
+        public int VallaId { get; set; }
+        public decimal PrecioAplicado { get; set; }
+        public virtual Valla? Valla { get; set; }
+    }
+}
