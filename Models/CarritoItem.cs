75e17c39352851e6b89650fba059461a64704018
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:17:01 -0500

    Crea el modelo CarritoItem

---
+++ Models/CarritoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VallaGestApi.Models
+{
+    public class CarritoItem
+    {
+        [Key]
+        public int CarritoItemId { get; set; }
+        public int UsuarioId { get; set; }
+        public int VallaId { get; set; }
+        public virtual Valla? Valla { get; set; }
+    }
+}
