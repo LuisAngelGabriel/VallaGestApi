4b1cc013787859a872985e9a57b9c23ec46de105
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:17:41 -0500

    Crea el AuthDTO

---
+++ DTO/AuthDto.cs
@@ -0,0 +1,24 @@
+﻿namespace VallaGestApi.DTO
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class RegistroDto
+    {
+        public string Nombre { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Rol { get; set; } = "Usuario";
+    }
+
+    public class UsuarioDto
+    {
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Rol { get; set; } = string.Empty;
+    }
+}
