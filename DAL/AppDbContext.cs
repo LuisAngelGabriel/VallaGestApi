b4f48b4912b6e7192f173146399bff917d44a78d
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2026-03-28 18:16:24 -0500

    Agrega los modelos nuevos al Context

--- DAL/AppDbContext.cs
+++ DAL/AppDbContext.cs
@@ -1,5 +1,5 @@
-﻿using VallaGestApi.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using VallaGestApi.Models;
 
 namespace VallaGestApi.DAL
 {
@@ -7,7 +7,13 @@ namespace VallaGestApi.DAL
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+        public DbSet<Usuario> Usuarios { get; set; }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Valla> Vallas { get; set; }
+        public DbSet<CarritoItem> CarritoItems { get; set; }
+        public DbSet<Orden> Ordenes { get; set; }
+        public DbSet<OrdenDetalle> OrdenDetalles { get; set; }
+        }
     }
-}
+} 
+
